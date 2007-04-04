7996c9e3ecd4933d5c56d44356f221cb917c7d79
Jeremy Katz <katzj@redhat.com>
2007-04-04 16:14:27 -0400

    add some sanity checking of usb sticks * need to ensure that the partition is active, otherwise, it likely won't be   booted * need to ensure there's an mbr.  otherwise, it definitely won't be booted * also, add the syslinux version check from Will Woods

--- creator/isotostick.sh
+++ creator/isotostick.sh
@@ -35,6 +35,95 @@ exitclean() {
     exit 1
 }
 
+getdisk() {
+    DEV=$1
+
+    p=$(udevinfo --query=path --name=$DEV)
+    if [ -e /sys/$p/device ]; then
+	device=$(basename /sys/$p)
+    else
+	device=$(basename $(readlink -f /sys/$p/../))
+    fi
+    if [ ! -e /sys/block/$device -o ! -e /dev/$device ]; then
+	echo "Error finding block device of $DEV.  Aborting!"
+	exitclean
+    fi
+
+    device="/dev/$device"
+}
+
+checkMBR() {
+    getdisk $1
+
+    bs=$(mktemp /tmp/bs.XXXXXX)
+    dd if=$device of=$bs bs=512 count=1 2>/dev/null || exit 2
+    
+    mbrword=$(hexdump -n 2 $bs |head -n 1|awk {'print $2;'})
+    rm -f $bs
+    if [ "$mbrword" = "0000" ]; then
+	echo "MBR appears to be blank."
+	echo "You can add an MBR to this device with "
+	echo "   # cat /usr/lib/syslinux/mbr.bin > $device"
+	exitclean
+    fi
+
+    return 0
+}
+
+checkPartActive() {
+    dev=$1
+    getdisk $dev
+    
+    # if we're installing to whole-disk and not a partition, then we 
+    # don't need to worry about being active
+    if [ "$dev" = "$device" ]; then
+	return
+    fi
+
+    if [ "$(/sbin/fdisk -l $device 2>/dev/null |grep $dev |awk {'print $2;'})" != "*" ]; then
+	echo "Partition isn't marked bootable!"
+	echo "You can mark the partition as bootable with "
+        echo "    # /sbin/parted $device"
+	echo "    (parted) toggle N boot"
+	exitclean
+    fi
+}
+
+checkFilesystem() {
+    dev=$1
+
+    USBFS=$(/lib/udev/vol_id -t $dev)
+    if [ "$USBFS" != "vfat" -a "$USBFS" != "msdos" -a "$USBFS" != "ext2" -a "$USBFS" != "ext3" ]; then
+	echo "USB filesystem must be vfat or ext[23]"
+	exitclean
+    fi
+
+    USBLABEL=$(/lib/udev/vol_id -u $dev)
+    if [ -n "$USBLABEL" ]; then 
+	USBLABEL="UUID=$USBLABEL" ; 
+    else
+	USBLABEL=$(/lib/udev/vol_id -l $dev)
+	if [ -n "$USBLABEL" ]; then 
+	    USBLABEL="LABEL=$USBLABEL" 
+	else
+	    echo "Need to have a filesystem label or UUID for your USB device"
+	    if [ "$USBFS" = "vfat" -o "$USBFS" = "msdos" ]; then
+		echo "Label can be set with /sbin/dosfslabel"
+	    elif [ "$USBFS" = "ext2" -o "$USBFS" = "ext3" ]; then
+		echo "Label can be set with /sbin/e2label"
+	    fi
+	    exitclean
+	fi
+    fi
+}
+
+checkSyslinuxVersion() {
+    if ! syslinux 2>&1 | grep -qe -d; then
+	echo "Your syslinux is too old for this."
+	exitclean
+    fi
+}
+
 if [ $(id -u) != 0 ]; then 
     echo "You need to be root to run this script"
     exit 1
@@ -61,24 +150,11 @@ mount -o loop $ISO $CDMNT || exitclean
 USBMNT=$(mktemp -d /media/usbdev.XXXXXX)
 mount $USBDEV $USBMNT || exitclean
 
-USBLABEL=$(/lib/udev/vol_id -u $USBDEV)
-if [ -n "$USBLABEL" ]; then 
-    USBLABEL="UUID=$USBLABEL" ; 
-else
-    USBLABEL=$(/lib/udev/vol_id -l $USBDEV)
-    if [ -n "$USBLABEL" ]; then 
-	USBLABEL="LABEL=$USBLABEL" 
-    else
-	echo "Need to have a filesystem label or UUID for your USB device"
-	exitclean
-    fi
-fi
-USBFS=$(/lib/udev/vol_id -t $USBDEV)
-if [ "$USBFS" != "vfat" -a "$USBFS" != "msdos" -a "$USBFS" != "ext2" -a "$USBFS" != "ext3" ]; then
-    echo "USB filesystem must be vfat or ext[23]"
-    exitclean
-fi
-
+# do some basic sanity checks.  
+checkSyslinuxVersion 
+checkFilesystem $USBDEV
+checkPartActive $USBDEV
+checkMBR $USBDEV
 
 trap exitclean SIGINT SIGTERM
 
