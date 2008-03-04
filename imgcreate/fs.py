e4d5d2b690db07b3656ca85a3262f135c559c3d9
Jeremy Katz <katzj@redhat.com>
2008-03-04 09:21:30 -0500

    Revert "Temporarily use 128 byte inodes to avoid hitting #434893"
    This reverts commit 04298ddc7bb8f0aef55a54b6c2367a6387c8100a.
    Note that if you have a version of e2fsprogs >= 1.40.4-7 and < 1.40.7-2,
    you will get missing xattrs from files during the resize operation.  Thus,
    an upgrade is recommended :)

--- imgcreate/fs.py
+++ imgcreate/fs.py
@@ -201,7 +201,7 @@ class SparseExtLoopbackMount(SparseLoopbackMount):
     def __format_filesystem(self):
         rc = subprocess.call(["/sbin/mkfs." + self.fstype,
                               "-F", "-L", self.fslabel,
-                              "-m", "1", "-b", str(self.blocksize), "-I128",
+                              "-m", "1", "-b", str(self.blocksize),
                               self.lofile,
                               str(self.size / self.blocksize)])
         if rc != 0:
