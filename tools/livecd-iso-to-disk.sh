a2f6bcec53e2c455cfcff76a4a297c31685034f1
Jeremy Katz <katzj@redhat.com>
2008-11-12 12:31:19 -0500

    Make initrd mem patch for OLPC conditional on firmware revision
    Make the olpc.fth patch for changing where the ramdisk gets loaded
    conditional on firmware revision since later firmwares already do
    this (Mitch Bradley, http://dev.laptop.org/ticket/8971#comment:2)

--- tools/livecd-iso-to-disk.sh
+++ tools/livecd-iso-to-disk.sh
@@ -547,14 +547,16 @@ if [ -n "$xo" ]; then
     if [ ! -d $USBMNT/boot ]; then mkdir -p $USBMNT/boot ; fi
     cat > $USBMNT/boot/olpc.fth <<EOF
 \ Boot script for USB boot
-patch 2drop erase claim-params
-: high-ramdisk  ( -- )
-   cv-load-ramdisk
-   h# 22c +lp l@ 1+   memory-limit  umin  /ramdisk - ffff.f000 and ( new-ramdisk-adr )
-   ramdisk-adr over  /ramdisk move                    ( new-ramdisk-adr )
-   to ramdisk-adr
-;
-' high-ramdisk to load-ramdisk
+hex  rom-pa fffc7 + 4 $number drop  h# 2e19 < [if]
+  patch 2drop erase claim-params
+  : high-ramdisk  ( -- )
+     cv-load-ramdisk
+     h# 22c +lp l@ 1+   memory-limit  umin  /ramdisk - ffff.f000 and ( new-ramdisk-adr )
+     ramdisk-adr over  /ramdisk move                    ( new-ramdisk-adr )
+     to ramdisk-adr
+  ;
+  ' high-ramdisk to load-ramdisk
+[then]
 
 : set-bootpath-dev  ( -- )
    " /chosen" find-package  if                       ( phandle )
