a30258704a30aac49ac1d5a2db3eb875a20eb628
Brian C. Lane <bcl@redhat.com>
2012-02-29 16:24:37 -0800

    livecd-iso-to-disk: Add 2MB slop to calculation

--- tools/livecd-iso-to-disk.sh
+++ tools/livecd-iso-to-disk.sh
@@ -428,7 +428,7 @@ createGPTLayout() {
     else
         p2_size=0
     fi
-    p1_size=$(($dev_size - 1 - $p2_size))
+    p1_size=$(($dev_size - 3 - $p2_size))
 
     if [ $p1_size -le 0 ]; then
         echo "Your device isn't big enough to hold $SRC"
@@ -481,7 +481,7 @@ createMSDOSLayout() {
     else
         p2_size=0
     fi
-    p1_size=$(($dev_size - 1 - $p2_size))
+    p1_size=$(($dev_size - 3 - $p2_size))
 
     if [ $p1_size -le 0 ]; then
         echo "Your device isn't big enough to hold $SRC"
@@ -538,7 +538,7 @@ createEXTFSLayout() {
     else
         p2_size=0
     fi
-    p1_size=$(($dev_size - 1 - $p2_size))
+    p1_size=$(($dev_size - 3 - $p2_size))
 
     if [ $p1_size -le 0 ]; then
         echo "Your device isn't big enough to hold $SRC"
